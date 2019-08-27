eb7df4726aff7ad720cb6725187930abd6077fdd
Gawah Chan <gawahchan@gmail.com>
2019-08-27 18:59:21 +0100

    Updates

---
+++ precache-manifest.fbc8a267dc89b1e2c4b769d23bc75694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bbb17728a28f6b645cdd5e03ffd48f",
+    "url": "/Gift-giver/index.html"
+  },
+  {
+    "revision": "feb5a3995b56e7ff95d2",
+    "url": "/Gift-giver/static/css/main.e787c491.chunk.css"
+  },
+  {
+    "revision": "8a3b9882969c2c0a17a4",
+    "url": "/Gift-giver/static/js/2.42bbabb9.chunk.js"
+  },
+  {
+    "revision": "feb5a3995b56e7ff95d2",
+    "url": "/Gift-giver/static/js/main.d377d9d5.chunk.js"
+  },
+  {
+    "revision": "536c9ebfbdce99ae20de",
+    "url": "/Gift-giver/static/js/runtime~main.fc5271c6.js"
+  }
+]);
